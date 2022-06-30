c623e2b6ba01174e169c0aa2f5c8e67dc963eb07
Oscar De La Torre Hernandez <delatorreoscar27@outlook.com>
2022-06-30 02:29:03 -0500

    feat: update index.ts file

--- index.ts
+++ index.ts
@@ -1,15 +1,43 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import {Express, NextFunction, Response, Request } from "express"
+import routes from "./src/5-presentation/webapi/routes";
+const dotenv = require('dotenv');
+const express = require('express');
+
 
 dotenv.config();
-
 const app: Express = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+// bodyparser
+app.use(express.json());
+
+app.use(routes)
+
+// === BOILERPLATE ===
+// Catch and send error messages
+app.use( (err: any, req: Request, res:Response, next: NextFunction) => {
+  if (err) {
+    console.error(err.message)
+    if (!err.statusCode) {
+      err.statusCode = 500
+    } // Set 500 server code error if statuscode not set
+    return res.status(err.statusCode).send({
+      statusCode: err.statusCode,
+      message: err.message
+    })
+  }
+
+  next()
+})
+
+// 404
+app.use((err: any, req: Request, res:Response, next: NextFunction) => {
+  res.status(404).json({
+    status: 'Page does not exist'
+  });
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+})
