47d57db9b81360eb4dbb02bb5cb8c4eca66ebc27
Oscar De La Torre Hernandez <delatorreoscar27@outlook.com>
2022-06-30 16:20:01 -0500

    feat: add singleton

--- branDI/tokens.ts
+++ branDI/tokens.ts
@@ -1,9 +1,13 @@
 import { token } from 'brandi';
 import { UserCommands } from '../src/2-application/users/userCommands';
+import { UserQueries } from '../src/2-application/users/userQueries';
 import { IUserGateway } from '../src/3.gateways/userGateway';
+import { IUserRepository } from '../src/3.gateways/userRepository';
 
 
 export const TOKENS = {
-  userCommands: token<UserCommands>('userCommands'),
   userGateway: token<IUserGateway>('userGateway'),
+  userCommands: token<UserCommands>('userCommands'),
+  userQueries: token<UserQueries>('userQueries'),
+  userRepository: token<IUserRepository>('userRepository'),
 };
---
+++ src/2-application/users/userQueries.ts
@@ -0,0 +1,19 @@
+import { injected } from "brandi";
+import { Observable } from "rxjs";
+import { TOKENS } from "../../../branDI/tokens";
+import { User } from "../../1-domain/entities";
+import { IUserRepository } from "../../3.gateways/userRepository";
+
+export class UserQueries {
+    constructor(private userRepository: IUserRepository){ }
+
+    getUsers(): Observable<User[]> {
+        return this.userRepository.get();
+    }
+
+    getById(id: string): Observable<User | null> {
+        return this.userRepository.getById(id);
+    }
+}
+
+injected(UserQueries, TOKENS.userRepository);
