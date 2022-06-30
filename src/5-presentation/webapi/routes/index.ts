86b1be6653bd7f2e4a5dcb3f1ed458c17411c2b4
Oscar De La Torre Hernandez <delatorreoscar27@outlook.com>
2022-06-30 16:23:26 -0500

    feat: add routes and controllers for user

--- src/5-presentation/webapi/routes/index.ts
+++ src/5-presentation/webapi/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import userRouter from './user';
-const express =  require('express');
+const express = require('express');
 
 const router: Router = express.Router()
 
 router
-  .post('/signup', userRouter.signIn);
+  .post('users/signup', userRouter.signIn)
+  .get('/users', userRouter.getUsers)
+  .get('/users/:id', userRouter.getUserById)
+  .put('/users', userRouter.updateUser)
+  .post('/users', userRouter.createNewUser)
+  .delete('/users/:id', userRouter.deleteUser);
 
 export default router;
--- src/5-presentation/webapi/routes/user.ts
+++ src/5-presentation/webapi/routes/user.ts
@@ -5,13 +5,13 @@ import { CreateUserDto } from "../../../2-application/users/dtos/createUserDto";
 import { take } from 'rxjs/operators'
 import { ErrorResponseViewModel } from '../../../2-application/errorResponseViewModel';
 import { UserCreatedViewModel } from '../../../2-application/users/viewModels/userCreatedViewModel';
+import { UpdateUserDto } from '../../../2-application/users/dtos/updateUserDto';
 
 const signIn = (req: Request, res: Response) => {
-
     const userCommands = container.get(TOKENS.userCommands);
 
     userCommands.createUser(req.body as CreateUserDto)
-    .pipe(take(1))
+        .pipe(take(1))
         .subscribe({
             next: (response: UserCreatedViewModel) => {
                 res.send(response)
@@ -22,8 +22,83 @@ const signIn = (req: Request, res: Response) => {
         });
 }
 
+const getUsers = (req: Request, res: Response) => {
+
+    const userQueries = container.get(TOKENS.userQueries);
+
+    userQueries.getUsers().subscribe({
+        next: (response) => {
+            res.send(response)
+        },
+        error: (error: ErrorResponseViewModel) => {
+            res.status(error.status).json(error);
+        }
+    });
+}
+
+const getUserById = (req: Request, res: Response) => {
+
+    const userQueries = container.get(TOKENS.userQueries);
+
+    userQueries.getById( req.params.id ).subscribe({
+        next: (response) => {
+                res.send(response)
+        },
+        error: (error: ErrorResponseViewModel) => {
+            res.status(500).json(error);
+        }
+    });
+}
+
+const createNewUser = (req: Request, res: Response) => {
+
+    const userCommands = container.get(TOKENS.userCommands);
+
+    userCommands.createNewUser( req.body as CreateUserDto ).subscribe({
+        next: (response) => {
+            res.send(response)
+        },
+        error: (error: ErrorResponseViewModel) => {
+            res.status(500).json({error: error.message, status: 500 });
+        }
+    });
+}
+
+const updateUser = (req: Request, res: Response) => {
+
+    const userCommands = container.get(TOKENS.userCommands);
+
+    userCommands.updateUser( req.body as UpdateUserDto ).subscribe({
+        next: (response) => {
+            res.send({ status: response })
+        },
+        error: (error: ErrorResponseViewModel) => {
+            res.status(500).json({error: error.message, status: 500 });
+        }
+    });
+}
+
+const deleteUser = (req: Request, res: Response) => {
+
+    const userCommands = container.get(TOKENS.userCommands);
+
+    userCommands.deleteUser( req.params.id ).subscribe({
+        next: (response) => {
+            res.send({ status: response })
+        },
+        error: (error: ErrorResponseViewModel) => {
+            res.status(500).json({error: error.message, status: 500 });
+        }
+    });
+}
+
 const router = {
-    signIn
+    signIn,
+    getUsers,
+    getUserById,
+    createNewUser,
+    updateUser,
+    deleteUser
 };
 
 export default router;
