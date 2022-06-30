6e3a2c16b99fd15b5a2e76fabee259ba20a83228
Oscar De La Torre Hernandez <delatorreoscar27@outlook.com>
2022-06-30 00:35:58 -0500

    fix: remove unused lines

--- src/5-presentation/webapi/routes/index.ts
+++ src/5-presentation/webapi/routes/index.ts
@@ -1,11 +1,5 @@
 import express from 'express'
-import { NextFunction, Request, Response } from 'express';
-import Container from 'typedi';
-import { UserRepository } from '../../../2-application/users';
-import { CreateUserDto } from '../../../2-application/users/dtos/createUserDto';
 import router from './user';
-
-const userRepository = Container.get(UserRepository);
 
 const userRouter  = express.Router()
   .post('/signup', router.signIn);
