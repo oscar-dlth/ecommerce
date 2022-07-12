05d55170607e3d9dfeb5b674a814c673b62a897c
Oscar De La Torre Hernandez <delatorreoscar27@outlook.com>
2022-07-12 00:30:14 -0500

    feat: add jest and supertest typescript unit testing set up.

---
+++ tests/users.spec.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import app from "../src/5-presentation/webapi";
+
+describe('GET users', ()=>{
+    test('should return status code 200', async ()=>{
+        const response = await request(app).get('/users').send();
+        expect(response.statusCode).toBe(200);
+    })
+
+    test('should return object with data as array of users', async ()=>{
+        const response = await request(app).get('/users').send();
+        expect(response.body.data).toBeInstanceOf(Array);
+    })
+    
+    test('should return status OK', async ()=>{
+        const response = await request(app).get('/users').send();
+        expect(response.body.status).toBeDefined();
+    })
+});
