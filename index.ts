37fc868d2355e006e9b056388e6c2bb1d39b2506
oscar-dlth <delatorreoscar27@outlook.com>
2023-03-15 15:49:57 -0600

    feat: update database to mysql

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import mongoose from "./src/4-infrastructure/db/mongoDB/conection";
 import db from "./src/4-infrastructure/db/mySql/sequelizer/models";
 import app from "./src/5-presentation/webapi";
 
@@ -7,8 +6,10 @@ import app from "./src/5-presentation/webapi";
 const PORT = process.env.PORT || 3000
 
 
-/*
-Mysql setup
+
+//Mysql setup
+
+
 
 db.sequelize.sync().then( ()=> {
   
@@ -19,22 +20,4 @@ db.sequelize.sync().then( ()=> {
   })
 
 });
-/*
 
-/*
- Mongo DB setup
-*/
-// Signal connection
-mongoose.connection.once('open', ()=>{
-  
-  app.listen(PORT, () => {
-    
-    console.log(`Listening on PORT: ${PORT}`);
-  
-  })
-
-}).on('error', (error) => {
-
-}).on('disconnected', () => {
-
-}) 
