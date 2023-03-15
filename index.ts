19eef4e27ae1632d5dbd11b3ba5d5b26504f5b72
oscar-dlth <delatorreoscar27@outlook.com>
2023-03-15 15:50:50 -0600

    fix: spaces

--- index.ts
+++ index.ts
@@ -1,16 +1,9 @@
 import db from "./src/4-infrastructure/db/mySql/sequelizer/models";
 import app from "./src/5-presentation/webapi";
 
-
-
 const PORT = process.env.PORT || 3000
 
-
-
 //Mysql setup
-
-
-
 db.sequelize.sync().then( ()=> {
   
   app.listen(PORT, () => {
