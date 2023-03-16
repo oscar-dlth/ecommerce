85f665b1cfabf65427343fbe7b6c594b7395a534
oscar-dlth <delatorreoscar27@outlook.com>
2023-03-16 12:55:35 -0600

    feat: add users seed

---
+++ src/4-infrastructure/db/mySql/sequelizer/seeders/20230316184345-users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      name: 'Oscar De La Torre',
+      nickName: 'Oscar123',
+      email: 'delatorreoscar81@gmail.com',
+      password: '0a0a486fbe0c1f52',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
+
