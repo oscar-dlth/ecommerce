3d90f808cca452615b91d9428c19e6cc1186aee6
oscar-dlth <delatorreoscar27@outlook.com>
2023-03-23 08:44:25 -0600

    fix: remove comments

--- src/4-infrastructure/db/mySql/sequelizer/seeders/20230317153832-roles.js
+++ src/4-infrastructure/db/mySql/sequelizer/seeders/20230317153832-roles.js
@@ -3,23 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
   }
 };
