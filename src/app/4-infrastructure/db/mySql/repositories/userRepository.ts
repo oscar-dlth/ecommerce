d079de34cfbaff37f32b1de3fa803e78bba5dcfd
oscar-dlth <delatorreoscar27@outlook.com>
2023-08-30 00:57:41 -0600

    udpate

--- src/app/4-infrastructure/db/mySql/repositories/userRepository.ts
+++ src/app/4-infrastructure/db/mySql/repositories/userRepository.ts
@@ -1,8 +1,8 @@
-import { injected } from "brandi";
-import { BaseRepository } from "./base/baseRepository";
-import db from "../sequelizer/models";
-import { User } from "@domain/entities/User";
 import { IUserRepository } from "@gateways/repositories/IUserRepository";
+import { BaseRepository } from "./base/baseRepository";
+import { User } from "@domain/entities/User";
+import db from "../sequelizer/models";
+import { injected } from "brandi";
 
 export class UserRepository extends BaseRepository<User> implements IUserRepository {
     constructor() {
