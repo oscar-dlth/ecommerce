6b461d263380400d3e07f82e6e62cc387cd47554
oscar-dlth <delatorreoscar27@outlook.com>
2023-08-30 01:24:16 -0600

    update

--- src/dependency-inyection/registerServices.ts
+++ src/dependency-inyection/registerServices.ts
@@ -1,8 +1,7 @@
 import { TOKENS } from "@dependency-inyection/tokens";
-import { CategoryRepository } from "@infrastructure/db/mySql/repositories/CategoryRepository";
-import { ProductRepository } from "@infrastructure/db/mySql/repositories/ProductRepository";
-import { UserRepository } from "@infrastructure/db/mySql/repositories/UserRepository";
-import { AuthService } from "@infrastructure/services/AuthService";
+import { CategoryRepository } from "@infrastructure/db/mySql/repositories/categoryRepository";
+import { ProductRepository } from "@infrastructure/db/mySql/repositories/productRepository";
+import { UserRepository } from "@infrastructure/db/mySql/repositories/userRepository";
 import { CategoryService } from "@infrastructure/services/CategoryService";
 import { ProductsService } from "@infrastructure/services/productsService";
 import { UsersService } from "@infrastructure/services/UserService";
