b53dcf01ebe785c555ad6c272b1f4f30e147fad3
oscar-dlth <delatorreoscar27@outlook.com>
2023-08-30 01:08:57 -0600

    update

--- src/dependency-inyection/registerServices.ts
+++ src/dependency-inyection/registerServices.ts
@@ -2,11 +2,11 @@ import { TOKENS } from "@dependency-inyection/tokens";
 import { CategoryRepository } from "@infrastructure/db/mySql/repositories/CategoryRepository";
 import { ProductRepository } from "@infrastructure/db/mySql/repositories/ProductRepository";
 import { AuthService } from "@infrastructure/services/AuthService";
-import { UserRepository } from "@infrastructure/db/mySql/repositories/UserRepository";
 import { CategoryService } from "@infrastructure/services/CategoryService";
 import { ProductsService } from "@infrastructure/services/productsService";
 import { UsersService } from "@infrastructure/services/UserService";
 import { Container } from "brandi";
+import { UserRepository } from "@infrastructure/db/mySql/repositories/UserRepository";
 
 export const registerServices = (container: Container) => {
   container
